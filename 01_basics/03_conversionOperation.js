6d88840b02a4fc2e7b6eb9dd074802ec43287a11
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-11 18:43:43 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+// let score = null
+
+// let valueInNumber = Number(score)
+// console.log(typeof score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = "sandeep"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
