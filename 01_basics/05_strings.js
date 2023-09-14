db12d1b44b54d2212b2e745ab8a4ffa512f778d2
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-14 16:35:55 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = 'sandeep'
+const repoCount = 50
+
+// console.log(name + repoCount); - Old way
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('sandeep') 
+console.log(gameName.toUpperCase());
+
+
