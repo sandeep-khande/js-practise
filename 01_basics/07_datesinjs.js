2fcf1c05b1702c6ffcab1808663155e97577c682
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-15 13:06:34 +0000

    dates

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,19 @@
+// dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(typeof myDate);
+
+let myCreateDate = new Date(2023, 2, 1)
+console.log(myCreateDate.toDateString());
+
+let myTimeStamp = Date.now() - to convert this milisecond into second / 1000
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+
