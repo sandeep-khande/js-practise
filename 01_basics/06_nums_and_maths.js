dd396bdd3946eec48aa5657bbbf3bc3e049ee919
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-15 12:28:49 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,20 @@
+const balance = new Number(100)
+console.log(balance);
+
+const score = 100
+console.log(score);
+
+console.log(typeof score);
+
+console.log(balance.toFixed(3));
+
+const otherNumber = 12.336
+console.log(otherNumber.toPrecision(4));
+
+const hunderds = 1000000
+console.log(hunderds.toLocaleString('en-IN'));
+
+// ++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
