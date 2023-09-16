c9adabade136cc54dc59cc418f0a7deebe5d3970
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-16 17:04:40 +0000

    objects 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,9 @@
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = { obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
