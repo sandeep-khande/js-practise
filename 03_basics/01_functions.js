f1db3d9bdd320aa0c196d8a10dfafce10c913546
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-17 07:15:41 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,27 @@
+
+function sayMyName (){
+    console.log("s");
+    console.log("a");
+    console.log("n");
+    console.log("d");
+    console.log("e");
+    console.log("e");
+    console.log("p");
+}
+
+sayMyName()
+
+function addTwoNumbers (number1, number2){
+    console.log(number1 + number2);
+}
+function addTwoNumbers (number1, number2){
+    let result = number1 + number2
+    return result
+}
+
+const result = addTwoNumbers(4, 5)
+console.log(result);
+
+function loginUserMessage(username){
+
+}
