66ec1ca9b5c7bc88a6c089558b9eb3a3a21ec8ce
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-17 12:50:40 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+if (true){
+    let a = 10
+    const b = 20
+    var c = 30
+    d = 40
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+function one(){
+    const username = "sandeep"
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+one()
+
