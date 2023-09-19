a1f1559a5e7651b95c94bd9e2cbe3b146aff40b2
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-19 11:58:04 +0000

    one

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,18 @@
+// if
+
+// let temp = 12
+
+// if (temp < 50){
+//     console.log("temperature is less than 50");
+// } 
+// else {
+//     console.log("temperatureis greater than 50");
+// }
+
+// let score = 200
+// if (score > 100){
+//     let power = 'fly'
+//     console.log("user power is", power);
+//     console.log(`user power is ${power}`);
+// }
+
