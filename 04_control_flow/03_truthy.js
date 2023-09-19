229cac3e85c446bb527f540831fa010810b752a0
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-19 11:58:38 +0000

    truthy

---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,9 @@
+const userEmail = "sa@gmail.com"
+
+if (userEmail){
+    console.log("user got email");
+} else {
+    console.log("user don't have email");
+}
+
+// in above example in condition we didn't check the condition if it is true or false but it is being assumed. 
