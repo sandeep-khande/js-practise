03d1d5ec3a2cc14e3d4a5d174ff15fa92027d440
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-20 07:56:59 +0000

    for

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,7 +1,25 @@
 // for
 
-for (let i = 0; i < array.length; i++) {
-    const element = i;
-    console.log(element);
+// for (let i = 0; i < array.length; i++) {
+//     const element = i;
+//     console.log(element);
+    
+// }
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log("Detected 5");
+//         break
+//     }
+//     console.log("value of i is", index);
+// }
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log("Detected 5");
+        continue
+    }
+    console.log("value of i is", index);
     
 }
