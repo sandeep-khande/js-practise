eb8b41715435f7a006f03363a34f1fc3129a3799
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-20 11:39:33 +0000

    while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while
+// let index = 0
+// while (index <= 12) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 3
+// }
+
+// let dch = ["batman", "superman", "flash"]
+// let arr = 0
+// while (arr < dch.length) {
+//     console.log(`my favorate superhero is ${dch[arr]}`);
+//     arr++
+// }
+
+let score = 0
+do {
+    if (score === 5) {
+        console.log(`special value`);
+    }
+
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
