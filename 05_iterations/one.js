092eec46f870143204cb93552282001880fae83a
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-20 08:00:13 +0000

    foor loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -15,11 +15,12 @@
 //     }
 //     console.log("value of i is", index);
 // }
-for (let index = 1; index <= 20; index++) {
+for (let index = 1; index <= 10; index++) {
     if (index == 5) {
         console.log("Detected 5");
         continue
     }
     console.log("value of i is", index);
+    console.log(`value of i is ${index}`);
     
 }
