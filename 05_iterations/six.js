b23453de8483df5ee23c9174ee54f0c248660f4d
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-09-20 18:08:08 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,13 @@
+// const lang = ["js", "c++", "pythan", "java"]
+
+// const language =lang.forEach ((item) => {
+//     console.log(item);
+// })
+
+// console.log(language);
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const mynum = num.filter( (nu) => nu > 4)
+
+console.log(mynum);
