8089da9f8dd073338d7a813eae91806fc8ea6a63
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-10-08 12:44:29 +0000

    promise methods and fetch

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -61,3 +61,14 @@ async function consumePromiseFive(){
 }
 
 consumePromiseFive()
+
+fetch("https://api.github.com/users/hiteshchoudhary")
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => {
+    console.log(error);
+})
