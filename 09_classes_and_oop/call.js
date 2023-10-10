5b57a272b611e246f290043e4042dab02e5453a8
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-10-10 06:54:32 +0000

    call

---
+++ 09_classes_and_oop/call.js
@@ -0,0 +1,13 @@
+function setUserName(username){
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username, email, password){
+    setUserName.call(this, username)
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai", "chai@ghjd.com", "123")
+console.log(chai);
