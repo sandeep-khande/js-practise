34cc138029fe2fed3354c53ef434e5688610fa42
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-10-10 16:55:24 +0000

    getters & setters syntax

--- 09_classes_and_oop/getters_setters.js
+++ 09_classes_and_oop/getters_setters.js
@@ -4,14 +4,23 @@ class user{
         this.password = password
     }
 
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email = value
+    }
+
     get password(){
-        return this._password.toUpperCase()
+        // return this._password.toUpperCase()
+        return `${this._password}sandeep`
     }
 
     set password(value){
-        this._password = value.toUpperCase()
+        this._password = value
     }
 }
 
 const sandeep = new user("s@khande.com", "san")
-console.log(sandeep.password);
+console.log(sandeep.email);
