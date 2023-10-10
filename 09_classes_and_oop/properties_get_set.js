67990531a89c4075bc229037593d0d15b9c43369
sandeep-khande <144322296+sandeep-khande@users.noreply.github.com>
2023-10-10 17:03:26 +0000

    function based syntax for getters and setters

---
+++ 09_classes_and_oop/properties_get_set.js
@@ -0,0 +1,16 @@
+function user(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, "email", {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+}
+
+const chai = new user("chai@tea.com", "459")
+console.log(chai.email);
